cfb3c268120638ff13edc5c31ca6c04a27f8193c
chiajoukuo <chiajoukuo@gmail.com>
2020-07-12 00:26:22 +0800

    step 3 JSON

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,6 +19,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -26,7 +27,12 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Chia-Jou!</h1>");
+		String[] messages = {"message 1", "message 2", "message 3"};
+		Gson gson = new Gson();
+    String json = gson.toJson(messages);
+		
+		response.setContentType("application/json;");
+    response.getWriter().println(json);
+		
   }
 }
